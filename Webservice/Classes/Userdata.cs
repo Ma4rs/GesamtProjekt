befc8475f65bd7cd4dba6883b823709c8aeff5d9
Ma4rs <mars.rally177@gmail.com>
2025-04-29 15:51:23 +0200

    Struktur Änderungen im Projekt auf gitlab

---
+++ Webservice/Classes/Userdata.cs
@@ -0,0 +1,20 @@
+﻿namespace C__Backend.Classes
+{
+    public class Userdata
+    {
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        public int? Credits { get; set; }
+        //public string PasswordHash { get; set; }
+        public Userdata(string username,string password, string email, int? credits)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+            Credits = credits;
+        }
+    }
+}
---
+++ Webservice/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using C__Backend.Classes;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+
+namespace C__Backend.Controllers
+{
+    [ApiController]
+    [Route("api/Casino/User")]
+    public class UserController : ControllerBase
+    {
+        [HttpPost("PostUserCredits")]
+        public IActionResult PostUserCredits([FromBody] Userdata data)
+        {
+            Debug.WriteLine($"Empfangen: {data.Username} hat {data.Credits} Credits.");
+            return Ok(new { message = "Daten empfangen", user = data.Username });
+            
+        }
+
+[HttpPost("GetData")]
+public async Task<IActionResult> GetData([FromBody] Userdata data)
+{
+    using (var context = new OnlineCasinoContext())
+    {
+        var user = await context.Users.FirstOrDefaultAsync(u => u.Email == data.Email);
+
+        if (user != null)
+        {
+            return Ok(new { credits = user.Credits, message = "User existiert bereits" });
+        }
+        else
+        {
+            var newUser = new User
+            {
+                Username = data.Username,
+                Email = data.Email,
+                PasswordHash = data.Password, // später Hash einbauen
+                Credits = 100 // Startcredits
+            };
+
+            context.Users.Add(newUser);
+            await context.SaveChangesAsync();
+
+            return Ok(new { credits = newUser.Credits, message = "Neuer User angelegt" });
+        }
+    }
+}
+
+
+        [HttpGet("GetCredits/{Username}")]
+        public string GetCredits(string username)
+        {
+            return username;
+        }
+    }
+}
