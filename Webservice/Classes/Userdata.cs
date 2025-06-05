4b0cba7ee16b62f4b7fe4cc7bffa11e1cbf43ed9
Alex Engel <alexander-a.engel@rohde-schwarz.com>
2025-06-05 08:48:05 +0200

    feat: use records

--- Webservice/Classes/Userdata.cs
+++ Webservice/Classes/Userdata.cs
@@ -1,19 +1,8 @@
 ﻿namespace C__Backend.Classes
 {
-    public class Userdata
+    public record Userdata(string Username, string Password, string Email)
     {
-
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Email { get; set; }
-
         // public int? Credits { get; set; }
         //public string PasswordHash { get; set; }
-        public Userdata(string username,string password, string email)
-        {
-            Username = username;
-            Password = password;
-            Email = email;
-        }
     }
 }
