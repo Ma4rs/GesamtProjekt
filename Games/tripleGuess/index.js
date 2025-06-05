3aa35e0659e689bb8cf973f90f6b43db2cb56f5b
Ma4rs <mars.rally177@gmail.com>
2025-06-05 12:13:26 +0200

    Betting Field blueprint

--- Games/tripleGuess/index.js
+++ Games/tripleGuess/index.js
@@ -1,16 +1,16 @@
+console.log("Script geladen!");
 const chips = document.querySelectorAll(".chip-btn");
-  let selectedChip = null;
+let selectedChip = null;
 
-  chips.forEach(chip => {
-    chip.addEventListener("click", () => {
-      if (selectedChip) {
-        selectedChip.classList.remove("active");
-      }
+chips.forEach(chip => {
+  chip.addEventListener("click", () => {
+    if (selectedChip) {
+      selectedChip.classList.remove("active");
+    }
+    chip.classList.add("active");
+    selectedChip = chip;
 
-      chip.classList.add("active");
-      selectedChip = chip;
-
-      const value = chip.dataset.value;
-      console.log("Ausgewählter Chip:", value);
-    });
+    const value = chip.dataset.value;
+    console.log("Ausgewählter Chip:", value);
   });
+});
